c2c617b330d0fd99ce2641e1e7f2693419f97301
Luis Venegas <crow.99@hotmail.com>
2021-04-08 03:38:44 -0500

    more scss

--- dist/index.fed36e32.js
+++ dist/index.fed36e32.js
@@ -140,9 +140,9 @@
       this[globalName] = mainExports;
     }
   }
-})({"zktgs":[function(require,module,exports) {
+})({"jN1W4":[function(require,module,exports) {
 var HMR_HOST = null;
-var HMR_PORT = 3001;
+var HMR_PORT = 3008;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d751713988987e9331980363e24189ce";
 module.bundle.HMR_BUNDLE_ID = "dee346deb9a7d3b4285adb59fed36e32";
@@ -443,6 +443,6 @@ id) /*: string*/
 
 },{}],"68Jxl":[function(require,module,exports) {
 
-},{}]},["zktgs","68Jxl"], "68Jxl", "parcelRequirec903")
+},{}]},["jN1W4","68Jxl"], "68Jxl", "parcelRequirec903")
 
 //# sourceMappingURL=index.fed36e32.js.map
