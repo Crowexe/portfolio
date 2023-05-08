012beef1449f139c57e587d40dce84d9519bec2b
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2023-05-08 01:55:58 -0600

    navbar working process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = () => {
+    
+    const [abrir, setAbrir] = useState(false);
 
-  return (
-    <>        
-        <nav className='navbar-conteiner'>
-            <ul className='navbar-list'>
-                <li><a className='navbar-item' href="#Home">HOME</a></li>
-                <li><a className='navbar-item' href="#">ABOUT</a></li>
-                <li><a className='navbar-item' href="#">PORTFOLIO</a></li>
-                <li><a className='navbar-item' href="#">CONTACT</a></li>
-            </ul>
-        </nav>
-    </>
-  )
+    const manejarClick = () => {
+      setAbrir(!abrir);
+    };
+
+    return (
+        <>   
+            <div className={`hamburguesa ${abrir ? "abrir" : ""}`} onClick={manejarClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav className="navbar-conteiner">
+                
+                <ul className='navbar-list'>
+                    <li><a className='navbar-item' href="#Home">HOME</a></li>
+                    <li><a className='navbar-item' href="#">ABOUT</a></li>
+                    <li><a className='navbar-item' href="#">PORTFOLIO</a></li>
+                    <li><a className='navbar-item' href="#">CONTACT</a></li>
+                </ul>
+  
+            </nav>
+        </>
+    )
 };
 
 export default Navbar
