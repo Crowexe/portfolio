421e5d7f259f5d5e5993408daa1736689afff209
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2023-05-29 01:19:59 -0600

    projects update

---
+++ src/App2.jsx
@@ -0,0 +1,160 @@
+import { useRef } from 'react';
+import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+
+
+const url = (name, wrap = false) =>
+  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`;
+
+
+function App() {
+
+    const parallax = useRef(null)
+
+    return (
+        <>
+            <Navbar />
+            <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+                <Parallax ref={parallax} pages={4}>
+                
+
+                    <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} >
+                    </ParallaxLayer>
+                    
+                    <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} >
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={4} speed={1} style={{ backgroundColor: '#805E73' }} >
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={0}
+                    speed={0}
+                    factor={3}
+                    style={{
+                        height: '100vh',
+                        backgroundImage: "url('https://i.imgur.com/nU5yj0X.png')",
+                        backgroundSize: 'cover',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flexDirection: 'column'
+                    }}
+                    >
+                        {/*<img src={'/wave.png'} style={{ position: 'absolute', bottom: '0', width: '100%', marginBottom:'-18rem'}} />*/}
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+              
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+    
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+                    <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+                    </ParallaxLayer>
+                    
+                    <ParallaxLayer offset={3.6} speed={0.4} style={{ opacity: 0.6 }}>
+                    <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={2.5}
+                    speed={-0.4}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pointerEvents: 'none',
+                    }}>
+       
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={3.5}
+                    speed={-0.4}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        pointerEvents: 'none',
+                    }}>
+       
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={2}
+                    speed={-0.3}
+                    />
+
+                    <ParallaxLayer
+                    offset={0}
+                    speed={0.1}
+                    onClick={() => parallax.current.scrollTo(1)}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <Home/>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={1}
+                    speed={0.1}
+                    onClick={() => parallax.current.scrollTo(2)}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <About/>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={2}
+                    speed={-0}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                    onClick={() => parallax.current.scrollTo(3)}>
+                    
+                        <Projects/>
+
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                    offset={3}
+                    speed={-0}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                    onClick={() => parallax.current.scrollTo(0)}>
+                    <img src={url('clients-main')} style={{ width: '40%' }} />
+                    </ParallaxLayer>
+                </Parallax>
+                </div>
+        </>
+    );
+}
+
+export default App;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,53 @@
-{/*import React from 'react';*/}
+import { useState } from 'react';
 import './Projects.css';
 
-const Projects = () => {
-  return (
-    <>
-        <div className="projects-conteiner">
-            <div className="card">
-                <div className="card-text">
-                    <h1 className="card-title">To do list</h1>
-                    <p className="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                    <div className="tools">ReactJS</div>
-                    <a className="btn-hover" href='https://to-do-list-crow.vercel.app/'>View Project</a>
-                </div>
-            </div>
-        </div>
-    </>
-  )
-}
+const Carousel = () => {
+  const carouselData = [
+    {
+      imageUrl: '/proyectos/to-do-list.png',
+      text: 'To do list', 
+      description: 'To do list app made with React',
+      link: 'https://to-do-list-crow.vercel.app/',
+    },
+    {
+      imageUrl: '/proyectos/spotify-clone.png',
+      text: 'Spotify clone', 
+      description: 'Spotify clone made with React',
+      link: 'https://spotify-clone-kappa.vercel.app/',
+    },
+    {
+      imageUrl: '/proyectos/to-do-list.png',
+      text: 'Chat online', 
+      description: 'Chat online made with React and Firebase',
+      link: 'https://chat-react-six.vercel.app/',
+    },
+  ];
 
-export default Projects;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? carouselData.length - 1 : prevSlide - 1));
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === carouselData.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  return (
+    <div className="carousel-container">
+       <button className="prev-button" onClick={handlePrevSlide}>
+        <i className="fa-solid fa-arrow-left" />
+      </button>
+      <div className="carousel-image" style={{ backgroundImage: `url(${carouselData[currentSlide].imageUrl})` }}>
+        <div className="carousel-text">{carouselData[currentSlide].text}</div>
+        <div className="carousel-description">{carouselData[currentSlide].description}</div>
+        <a className="carousel-link" href={carouselData[currentSlide].link} target="_blank" rel="noopener noreferrer">View project</a>
+      </div>
+      <button className="next-button" onClick={handleNextSlide}>
+        <i className="fa-solid fa-arrow-right" />
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
