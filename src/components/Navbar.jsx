9f547e2941b9765042170719ffa2424da60a164a
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2023-05-29 01:26:44 -0600

    project added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 import './Navbar.css';
 
-
 const Navbar = () => {
 
     return (
         <>   
-            <nav className="navbar-conteiner">
-                
+            <nav className="navbar-conteiner">                
                 <ul className='navbar-list'>
                     <li><a className='navbar-item' href="https://www.linkedin.com/in/venevil/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"/></a></li>
                     <li><a className='navbar-item' href="https://github.com/Crowexe" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"/></a></li>
                     <li><a className='navbar-item' href="https://www.instagram.com/crow.exe_/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"/></a></li>
                     <li><a className='navbar-item' href="https://www.twitch.tv/onicrow_" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitch"/></a></li>
-                </ul>
-  
+                </ul>  
             </nav>
         </>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,52 +2,58 @@ import { useState } from 'react';
 import './Projects.css';
 
 const Carousel = () => {
-  const carouselData = [
-    {
-      imageUrl: '/proyectos/to-do-list.png',
-      text: 'To do list', 
-      description: 'To do list app made with React',
-      link: 'https://to-do-list-crow.vercel.app/',
-    },
-    {
-      imageUrl: '/proyectos/spotify-clone.png',
-      text: 'Spotify clone', 
-      description: 'Spotify clone made with React',
-      link: 'https://spotify-clone-kappa.vercel.app/',
-    },
-    {
-      imageUrl: '/proyectos/to-do-list.png',
-      text: 'Chat online', 
-      description: 'Chat online made with React and Firebase',
-      link: 'https://chat-react-six.vercel.app/',
-    },
-  ];
+    const carouselData = [
+        {
+            imageUrl: '/proyectos/to-do-list.png',
+            text: 'To do list', 
+            description: 'To do list app made with React',
+            link: 'https://to-do-list-crow.vercel.app/',
+        },
+        {
+            imageUrl: '/proyectos/spotify-clone.png',
+            text: 'Spotify clone', 
+            description: 'Spotify clone made with React',
+            link: 'https://spotify-clone-kappa.vercel.app/',
+        },
+        {
+            imageUrl: '/proyectos/to-do-list.png',
+            text: 'Chat online', 
+            description: 'Chat online made with React and Firebase',
+            link: 'https://chat-react-six.vercel.app/',
+        },
+        {
+            imageUrl: '/proyectos/01-citas-veterinaria.png',
+            text: 'Follow-up of veterinary patients',
+            description: 'Application for monitoring patients of a veterinary made with React', 
+            link: 'https://01-citas-veterinaria.vercel.app/',
+        },
+    ];
 
-  const [currentSlide, setCurrentSlide] = useState(0);
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-  const handlePrevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === 0 ? carouselData.length - 1 : prevSlide - 1));
-  };
+    const handlePrevSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? carouselData.length - 1 : prevSlide - 1));
+    };
 
-  const handleNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === carouselData.length - 1 ? 0 : prevSlide + 1));
-  };
+    const handleNextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === carouselData.length - 1 ? 0 : prevSlide + 1));
+    };
 
-  return (
-    <div className="carousel-container">
-       <button className="prev-button" onClick={handlePrevSlide}>
-        <i className="fa-solid fa-arrow-left" />
-      </button>
-      <div className="carousel-image" style={{ backgroundImage: `url(${carouselData[currentSlide].imageUrl})` }}>
-        <div className="carousel-text">{carouselData[currentSlide].text}</div>
-        <div className="carousel-description">{carouselData[currentSlide].description}</div>
-        <a className="carousel-link" href={carouselData[currentSlide].link} target="_blank" rel="noopener noreferrer">View project</a>
-      </div>
-      <button className="next-button" onClick={handleNextSlide}>
-        <i className="fa-solid fa-arrow-right" />
-      </button>
-    </div>
-  );
+    return (
+        <div className="carousel-container">
+            <button className="prev-button" onClick={handlePrevSlide}>
+                <i className="fa-solid fa-arrow-left" />
+            </button>
+            <div className="carousel-image" style={{ backgroundImage: `url(${carouselData[currentSlide].imageUrl})` }}>
+                <div className="carousel-text">{carouselData[currentSlide].text}</div>
+                <div className="carousel-description">{carouselData[currentSlide].description}</div>
+                <a className="carousel-link" href={carouselData[currentSlide].link} target="_blank" rel="noopener noreferrer">View project</a>
+            </div>
+            <button className="next-button" onClick={handleNextSlide}>
+                <i className="fa-solid fa-arrow-right" />
+            </button>
+        </div>
+    );
 };
 
 export default Carousel;
